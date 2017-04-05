0a195b82ae2fd141d985266c0e2709c769b4a54c
EC2 Default User <ec2-user@ip-172-31-3-243.eu-central-1.compute.internal>
2017-04-05 13:00:31 +0000

    finished lab12

--- lab12/library/set_max.py
+++ lab12/library/set_max.py
@@ -1,4 +1,7 @@
 #! /usr/bin/python
+from ansible.module_utils.basic import *
+import json
+
 def set_max_files(namesString, check_mode):
   filenames = namesString.split(",")
   files = map(lambda f: open(f, 'r') , filenames) 
@@ -16,3 +19,16 @@ def set_max_files(namesString, check_mode):
   
   return len(fileNamesToChange) > 0
 
+def main():
+ arg_spec = dict(
+    files = dict(requires=True)
+ ) 
+ module = AnsibleModule(argument_spec = arg_spec, supports_check_mode = True)
+
+ changed = set_max_files(module.params['files'], module.check_mode)
+
+ module.exit_json(changed = changed)
+
+if __name__ == '__main__': 
+	main()
+ 
